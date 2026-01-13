f1ff1aece9b475ea4b064f6441643fdf7fdec341
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-13 14:55:17 -0300

    Removendo componente de loader

--- client/src/components/Carregando.jsx
+++
@@ -1,5 +0,0 @@
-export function Loader() {
-    return (
-        <div className="loader absolute top-1/2 left-1/2 -translate-1/2"></div>
-    );
-};
--- client/src/screens/Teste.jsx
+++
@@ -1,7 +0,0 @@
-import { RelatorioClientes } from "../components/relatorios/RelatorioClientes";
-
-export function Teste() {
-    return (
-        <RelatorioClientes/>
-    );
-}
