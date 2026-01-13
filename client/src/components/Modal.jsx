9681c5c17e6110f61a8a3afb4326ff68cc68101e
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-13 17:11:20 -0300

    Adicionando animação de entrada e saída no modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,19 +1,28 @@
 import { createPortal } from "react-dom";
 import { CardBase } from "./cards/CardBase";
 import { X } from "lucide-react";
+import { useState } from "react";
 
 export function Modal({ titulo, mensagem, className = "", children , aberto, onFechar }) {
     const modalRoot = document.getElementById("modal-root");
+    const [ isExiting, setIsExiting ] = useState(false);
+
+    const handleAnimationEnd = () => {
+        if (isExiting) {
+            setIsExiting(false);
+            onFechar();
+        }
+    };
 
     if (!modalRoot) return null;
     if (!aberto) return null;
 
     return createPortal(
-        <div className="fixed inset-0 z-50 bg-gray-700/80 flex items-center justify-center">
-            <CardBase className={`relative w-100 bg-white ${className}`}>
+        <div className="fixed inset-0 z-50 bg-gray-700/80 backdrop-blur-xs flex items-center justify-center">
+            <CardBase onAnimationEnd={handleAnimationEnd}  className={`relative w-100 bg-white ${className} ${isExiting ? "animate-[fecharModal_1s_ease_forwards]" : "animate-[abrirModal_1s_ease_forwards]"}`}>
                 <div className="flex justify-between items-center">
                     <h2>{titulo ?? 'Aviso'}</h2>
-                    <X onClick={onFechar} className="cursor-pointer" />
+                    <X onClick={() => setIsExiting(true)} className="cursor-pointer" />
                 </div>
 
                 <hr className="border-gray-300" />
--- client/src/components/cards/CardBase.jsx
+++ client/src/components/cards/CardBase.jsx
@@ -1,6 +1,6 @@
-export function CardBase({children, className}) {
+export function CardBase({children, className, onAnimationEnd}) {
     return (
-        <div className={`p-6 rounded-md shadow-md border border-gray-200 ${className}`}>
+        <div className={`p-6 rounded-md shadow-md border border-gray-200 ${className}`} onAnimationEnd={onAnimationEnd}>
             {children}
         </div>
     );
